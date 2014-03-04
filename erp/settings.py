d375343e91f16f8c25c7ec033b6d844b136ad2db
Derek Musselmann <derek@disflux.com>
2014-03-04 09:00:20 -0600

    Move settings to a local copy

--- erp/settings.py
+++ erp/settings.py
@@ -36,6 +36,12 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Third party apps
+
+    # Local Apps
+    'inventory',
+    
 )
 
 MIDDLEWARE_CLASSES = (
